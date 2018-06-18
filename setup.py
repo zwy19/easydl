70c06581dfcb1e4dfe582d0743975bcbd8fe4fb5
You Kaichao <youkaichao@126.com>
2018-06-18 22:46:36 +0800

    add conda support for pytorch

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(name='easydl',
       url='https://github.com/thuml/easydl',
       license='MIT',
       packages=['easydl','easydl.tf', 'easydl.common', 'easydl.pytorch'],
-      install_requires=['tensorflow','tensorlayer','tensorpack','matplotlib','pathlib2','pytreebank','pytorch', 'torchvision'],
+      install_requires=['tensorflow','tensorlayer','tensorpack','matplotlib','pathlib2','pytreebank'],
       entry_points={
         'console_scripts': [
                 'runTask= easydl:runTask'],
