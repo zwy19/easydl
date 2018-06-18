e4c9e71ff6789c267afe7cfdb1e9ec6b2aa93c70
You Kaichao <youkaichao@126.com>
2018-06-18 22:20:30 +0800

    remove numpy and scipy dependency(they are required by tensorlayer)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(name='easydl',
       url='https://github.com/thuml/easydl',
       license='MIT',
       packages=['easydl','easydl.tf', 'easydl.common', 'easydl.pytorch'],
-      install_requires=['tensorflow','tensorlayer','numpy','tensorpack','scipy','matplotlib','pathlib2','pytreebank'],
+      install_requires=['tensorflow','tensorlayer','tensorpack','matplotlib','pathlib2','pytreebank','pytorch', 'torchvision'],
       entry_points={
         'console_scripts': [
                 'runTask= easydl:runTask'],
