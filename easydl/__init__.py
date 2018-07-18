758847face950420c08796889072a580f0dd752c
You Kaichao <youkaichao@gmail.com>
2018-07-18 09:13:26 +0800

    print traceback at import error

--- easydl/__init__.py
+++ easydl/__init__.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import traceback
 
 sys.path.append(os.path.abspath('./'))
 from common import *
@@ -9,12 +10,14 @@ try:
     from pytorch import *
 except ImportError as e:
     print('pytorch not available!')
+    traceback.print_exc()
 
 try:
     import tensorflow
     from tf import *
 except ImportError as e:
     print('tensorflow not available!')
+    traceback.print_exc()
 
 import warnings
 
