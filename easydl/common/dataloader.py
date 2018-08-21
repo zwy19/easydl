305e98984a4e23c7460fbcbcf3b700f6fa197097
You Kaichao <youkaichao@gmail.com>
2018-08-21 14:48:06 +0800

    bug fix: cifar100 dataset

--- easydl/common/dataloader.py
+++ easydl/common/dataloader.py
@@ -554,12 +554,14 @@ class Cifar100Dataset(InMemoryImageDataset):
     def _fill_data(self):
         self.cifar100 = cPickle.load(open(os.path.join(self.root_dir, 'cifar-100-python/train'), 'rb'))
         self.x_train = self.cifar100['data']
-        self.x_train.resize((self.x_train.shape[0], 32, 32, 3))
+        self.x_train.resize((self.x_train.shape[0], 3, 32, 32))
+        self.x_train = np.transpose(self.x_train, [0, 2, 3, 1])
         self.y_train = self.cifar100['fine_labels']
 
         self.cifar100_test = cPickle.load(open(os.path.join(self.root_dir, 'cifar-100-python/test'), 'rb'))
         self.x_test = self.cifar100_test['data']
-        self.x_test.resize((self.x_test.shape[0], 32, 32, 3))
+        self.x_test.resize((self.x_test.shape[0], 3, 32, 32))
+        self.x_test = np.transpose(self.x_test, [0, 2, 3, 1])
         self.y_test = self.cifar100_test['fine_labels']
 
         self.current_x = self.x_train if self.is_train else self.x_test
