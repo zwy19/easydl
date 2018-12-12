8f607db43d5c945e61567eb38e2f576521524a40
You Kaichao <youkaichao@gmail.com>
2018-12-12 14:02:12 +0800

    rename file to resolve conflict

--- easydl/pytorch/__init__.py
+++ easydl/pytorch/__init__.py
@@ -2,6 +2,6 @@ import os
 import sys
 sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
-from pytorch import *
+from utils import *
 
 sys.path.remove(os.path.dirname(os.path.abspath(__file__)))
